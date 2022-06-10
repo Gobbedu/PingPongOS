6678f1b43c0351422e575d8a975577f442ae51cd
gobbedu <gobbolegal@gmail.com>
2022-06-10 10:46:29 -0300

    remove relatorios

--- relatorios/relatorio/exemplo.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (int argc, char *argv[])
-{
-   int i ;                           // uma variavel local
-
-   for (i=0; i< 100; i++)            // um laco for
-      printf ("i vale %d\n", i) ;    // uma saida na tela
-}
